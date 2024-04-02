19274a6e8555d062030039f9f1a30111a15289a7
Manuel Wonda <Manuel Wonda>
2024-04-02 11:00:39 -0400

    Commit initial de mon projet

---
+++ main.cs
@@ -0,0 +1,8 @@
+internal class Program
+{
+    private static void Main (string[] args)
+    {
+        Console.WriteLine("Hello, Github! Je m'appelle Manuel Luther Wonda Gayong Lonkeng")
+    }
+
+}
